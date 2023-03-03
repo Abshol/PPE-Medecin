46e18ec76f62d0629fd28863c89531515e22f677
arenaut <abshol66@gmail.com>
2023-03-03 14:41:37 +0100

    update

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([48.822915, 2.271123], 14);
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+// var marker = L.marker([51.5, -0.09]).addTo(map);
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03', 
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#f03', 
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
 
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(map);
 
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");

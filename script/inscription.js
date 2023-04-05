34b4cdcf4a0eeeeb743d1414a954db7290f546b5
arenaut <abshol66@gmail.com>
2023-04-05 14:27:03 +0200

    update

---
+++ script/inscription.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    
+    $(document).on("click", "#sub", function(event) {
+        var nom = $("#nomPatient").val();
+        var prenom = $("#prenomPatient").val();
+        var rue = $("#ruePatient").val();
+        var cp = $("#cpPatient").val();
+        var ville = $("#villePatient").val();
+        var tel = $("#telPatient").val();
+        var login = $("#loginPatient").val();
+        var mdp = $("#mdpPatient").val();
+
+        var continu = true;
+        if (nom == "" || prenom == "" || rue == "" || cp == "" || ville == "" || tel == "" || login == "" || mdp == "") {
+            continu = false;
+        }
+        if (continu) {
+            $.ajax({
+                method: "POST",
+                url: "http://172.19.0.74/API",
+                data: JSON.stringify({nomPatient: nom, prenomPatient: prenom, ruePatient: rue, cpPatient: cp, villePatient: ville, telPatient: tel, loginPatient: login, mdpPatient: mdp})
+            })
+            .done(function(data, textStatus, jqXHR) {
+                alert("Votre compte a bien été créé");
+                console.log(data); // Réponse HTTP (body)
+                console.log(textStatus); // Statut de la requête AJAX
+                console.log(jqXHR); // Objet jqXHR (infos de la requête AJAX)
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                alert("Erreur lors de l'envoi des données");
+                console.log(jqXHR); // Objet jqXHR (infos de la requête AJAX)
+                console.log(textStatus); // Statut de la requête AJAX
+                console.log(errorThrown); // = statusText (type de l’erreur)
+            });
+        }
+        else {
+            alert("Merci de bien renseigner toutes les données");
+        }
+    });
+        
+});
--- script/map.js
+++ script/map.js
@@ -6,7 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // var circle = L.circle([51.508, -0.11], {
 //     color: 'red',
